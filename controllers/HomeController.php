f1e57719cb42d0adc9c803112a242fc49869c42d
DannyGuevara1 <69401899+DannyGuevara1@users.noreply.github.com>
2022-10-05 16:45:53 -0600

    TargetasAleatoriasConNumerosRepetidos

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -6,6 +6,7 @@ class HomeController
     {
         require_once 'model/homeM.php';
         require_once 'libs/animales.php';
+        $mamiferoArr = array();
         $perro = new Perro(
             'Pitbul',
             'es un perro muy agresivo',
@@ -21,6 +22,10 @@ class HomeController
             'es una animal muy grande que vivi en africa',
             '../img/elefante.jpeg'
         );
+        array_push($mamiferoArr,$perro);
+        array_push($mamiferoArr,$gato);
+        array_push($mamiferoArr,$elefante);
+        $mamiferoSize = sizeof($mamiferoArr);
         $home = new Home;
         $showhome = $home->showHome();
         require_once 'views/' . $showhome;
